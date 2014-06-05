ae89dae11cb0e32f31288dcea02a82facf9f3e9a
Christos Mantas <cmantas@cslab.ece.ntua.gr>
2014-06-05 13:13:33 +0300

    versioning, new resources schema testing

--- resources/create_db.sh
+++ resources/create_db.sh
@@ -1,10 +1,10 @@
-echo "use password 'celar-user'
-	then run command
+echo "run commands:
+	DROP DATABASE celardb;
 	CREATE DATABASE celardb;
-
-
+	\q
 "
 
 psql -U celaruser postgres
-
+echo "use password celar-user
+"
 psql -U celaruser -d celardb -a -f celardb.psql
